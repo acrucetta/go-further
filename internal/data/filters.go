14c31cb66189f2985cb618953001864bf56b4e27
andres crucetta <andres.crucetta@waltzhealth.com>
2025-09-10 09:44:27 -0700

    adding sorting to pg query

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,12 +1,31 @@
 package data
 
-import "greenlight.alexedwards.net/internal/validator"
+import (
+	"slices"
+	"strings"
+
+	"greenlight.alexedwards.net/internal/validator"
+)
 
 type Filters struct {
 	Page         int
 	PageSize     int
 	Sort         string
 	SortSafelist []string
+}
+
+func (f Filters) sortColumn() string {
+	if slices.Contains(f.SortSafelist, f.Sort) {
+		return strings.TrimPrefix(f.Sort, "-")
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
 }
 
 func ValidateFilters(v *validator.Validator, f Filters) {
